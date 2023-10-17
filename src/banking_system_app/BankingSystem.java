fbb17f93cb57af8672f6b9f6f1d98d4de450c2cc
mn <mn.de@outlookl.com>
2023-10-17 13:40:08 +0200

    Bank System

--- src/banking_system_app/BankingSystem.java
+++ src/banking_system_app/BankingSystem.java
@@ -2,13 +2,13 @@ package banking_system_app;
 
 import java.util.*;
 
-class InsufficientFundsException extends Exception{
+class InsufficientFundsException extends Exception {
 	public InsufficientFundsException(String message) {
 		super(message);
 	}
 }
 
-class InvalidPINException extends Exception{
+class InvalidPINException extends Exception {
 	public InvalidPINException(String message) {
 		super(message);
 	}
@@ -18,7 +18,7 @@ class BankAccount {
 	private int accountNumber;
 	private double balance;
 	private int pin;
-	
+
 	public BankAccount(int accountNumber, double balance, int pin) {
 		this.accountNumber = accountNumber;
 		this.balance = balance;
@@ -36,73 +36,69 @@ class BankAccount {
 	public int getPin() {
 		return pin;
 	}
-	
+
 	public void withdraw(double amount) throws InsufficientFundsException {
-		if(amount > balance) {
-			throw new InsufficientFundsException("Insufficient funds " + accountNumber);
+		if (amount > balance) {
+			// If the withdrawal amount is more than the balance, throw an exception.
+			throw new InsufficientFundsException("Insufficient funds for account " + accountNumber);
 		}
 		balance -= amount;
 	}
-	
+
 	public void deposit(double amount) {
 		balance += amount;
 	}
 }
 
-
 public class BankingSystem {
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		BankAccount[] account = {new BankAccount(123, 2500.0, 321), new BankAccount(456, 55500.0, 654), new BankAccount(789, 22200.0, 987), new BankAccount(147, 56800.0, 741)};
-		
-		try{
+		BankAccount[] accounts = { new BankAccount(123, 2500.0, 321), new BankAccount(456, 55500.0, 654),
+				new BankAccount(789, 22200.0, 987), new BankAccount(147, 56800.0, 741) };
+		try {
 			System.out.println("Enter your account number: ");
 			int accNr = scanner.nextInt();
 			System.out.println("Enter your PIN: ");
-			int enPin = scanner.nextInt();
-			
+			int enteredPin = scanner.nextInt();
 			BankAccount yourAccount = null;
-			
-			for(BankAccount acc : account) {
-				if(acc.getAccountNumber() == accNr) {
+			for (BankAccount acc : accounts) {
+				if (acc.getAccountNumber() == accNr) {
+					// If the account number matches, assign yourAccount to this account.
 					yourAccount = acc;
 					break;
 				}
 			}
-			
-			if(yourAccount == null) {
-				throw new IllegalArgumentException("Account number not exist.");
+			if (yourAccount == null) {
+				// If no account was found with the entered account number, throw an exception.
+				throw new IllegalArgumentException("Account number does not exist.");
 			}
-			if(enPin != yourAccount.getPin()) {
-				throw new InvalidPINException("Wrong PIN " + accNr);
+			if (enteredPin != yourAccount.getPin()) {
+				// If the entered PIN does not match the account's PIN, throw an exception.
+				throw new InvalidPINException("Wrong PIN for account " + accNr);
 			}
-			
 			System.out.println("1. Withdraw");
 			System.out.println("2. Deposit");
 			int select = scanner.nextInt();
-			
-			if(select == 1) {
+			if (select == 1) {
 				System.out.println("Withdraw amount: ");
 				double withdraw = scanner.nextDouble();
 				yourAccount.withdraw(withdraw);
 				System.out.println("Success. New balance: €" + yourAccount.getBalance());
-			}else if(select == 2) {
+			} else if (select == 2) {
 				System.out.println("Enter deposit amount: ");
 				double deposit = scanner.nextDouble();
 				yourAccount.deposit(deposit);
 				System.out.println("Success. New balance: €" + yourAccount.getBalance());
-			}else {
-				System.out.println("Wrong select.");
+			} else {
+				// If an invalid selection is made, print a message.
+				System.out.println("Wrong selection.");
 			}
-			
-		}catch(IllegalArgumentException | InvalidPINException | InsufficientFundsException e) {
+		} catch (IllegalArgumentException | InvalidPINException | InsufficientFundsException e) {
+			// Catch and print exceptions.
 			System.err.println("Error: " + e.getMessage());
-		}finally {
+		} finally {
+			// Close the scanner in the finally block.
 			scanner.close();
 		}
-
 	}
-
 }
